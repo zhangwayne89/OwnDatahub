06681de63daff1ac52d0606a8c8a4df57334ab9b
zhangcongwayne <zhangcong@chinacscs.com>
2018-03-14 13:41:47 +0800

    commit

--- 2.项目/7.模型入库自动化/数据备份/fdc_rating_model.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table rating_model...
-set feedback off
-set define off
-insert into rating_model (ID, CODE, NAME, CLIENT_ID, VALID_FROM_DATE, VALID_TO_DATE, MS_TYPE, TYPE, VERSION, IS_ACTIVE, ISDEL, CREATION_TIME)
-values (500000000004103, 'RE', '·¿µØ²ú', 5, null, null, 'LOGIT_MAPPING', 'STATISTICS', 1, 1, 0, '2017-06-24 22:49:28:000000');
-
-prompt Done.
--- 2.项目/8.上线前清理/重置测试用户数据.sql
+++
@@ -1,6 +0,0 @@
-update bond_rating_record set updt_by=0 where updt_by=662 ;
-update bond_rating_factor set updt_by=0 where updt_by=662 ; 
-update bond_rating_xw set updt_by=0 where updt_by=662 ;
-update bond_rating_display set updt_by=0 where updt_by=662 ;
-update rating_display set updt_by=0 where updt_by=662 ;
-update rating_record set user_id=0 where user_id=662 ;
